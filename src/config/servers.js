a3254410bbe9ceb0be6e2f0c66a76b79f7a6912b
Vortexsz <118730021+Vortexsz@users.noreply.github.com>
2022-12-13 08:49:16 -0500

    Update servers.js

--- src/config/servers.js
+++ src/config/servers.js
@@ -3,23 +3,13 @@ export const servers = [
 		"title": "Popular Servers",
 		"servers": [
 			{
-				"name": "Hypixel",
-				"host": "mc.hypixel.net",
+				"name": "YDupe",
+				"host": "YDupe.minehut.gg",
 				"port": 25565
 			},
 			{
 				"name": "Minehut",
 				"host": "minehut.com",
-				"port": 25565
-			},
-			{
-				"name": "Wynncraft",
-				"host": "play.wynncraft.com",
-				"port": 25565
-			},
-			{
-				"name": "CubeCraft",
-				"host": "play.cubecraft.net",
 				"port": 25565
 			}
 		]
